e0419f2da893f3ec1a1fe5256e56c3ba6f36733e
Ted Lo <teedsingyau@gmail.com>
2025-08-27 10:23:54 +0000

    Mainrun auto checkpoint

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-python3 train.py \
-  --block_size 64 \
-  --lr 0.006 \
-  --n_layer 4 \
-  --dropout 0.1 \
-  --weight_decay 0.1 \
-  --d_model 256 \
-  --batch_size 16
 
-# for bs in 64 128 256; do
-#   for lr in 0.006 0.005 0.004 0.003; do
-#     for n_layer in 4 6 8; do
-#       for dropout in 0.1 0.2 0.3; do
-#         for weight_decay in 0.1 0.01 0.001; do
-#           for d_model in 128 256 512; do
-#             for batch_size in 16 32 64; do
-#               echo "Running with bs=$bs, lr=$lr, n_layer=$n_layer, dropout=$dropout, weight_decay=$weight_decay, d_model=$d_model, batch_size=$batch_size"
-#               python3 .\mainrune\train.py \
-#                 --block_size $bs \
-#                 --lr $lr \
-#                 --n_layer $n_layer \
-#                 --dropout $dropout \
-#                 --weight_decay $weight_decay \
-#                 --d_model $d_model \
-#                 --batch_size $batch_size
-#             done
-#           done
-#         done
-#       done
-#     done
-#   done
-# done
+for bs in 64 128 256; do
+  for lr in 0.006 0.005 0.004 0.003; do
+    for n_layer in 4 6 8; do
+      for dropout in 0.1 0.2 0.3; do
+        for weight_decay in 0.1 0.01 0.001; do
+          for d_model in 128 256 512; do
+            for batch_size in 16 32 64; do
+                echo "Running with bs=$bs, lr=$lr, n_layer=$n_layer, dropout=$dropout, weight_decay=$weight_decay, d_model=$d_model, batch_size=$batch_size"
+                python3 train.py \
+                    --block_size $bs \
+                    --lr $lr \
+                    --n_layer $n_layer \
+                    --dropout $dropout \
+                    --weight_decay $weight_decay \
+                    --d_model $d_model \
+                    --batch_size $batch_size
+
+            done
+          done
+        done
+      done
+    done
+  done
+done
