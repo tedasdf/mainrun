b2a495c7695350a34048bdf4be1e0c46d2c612e5
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-08-28 19:37:53 +1000

    Update train.sh

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-for bs in 64 128 256; do
-  for lr in 0.006 0.005 0.004 0.003; do
-    for n_layer in 4 6 8; do
-      for dropout in 0.1 0.2 0.3; do
-        for weight_decay in 0.1 0.01 0.001; do
-          for d_model in 128 256 512; do
-            for batch_size in 16 32 64; do
-                echo "Running with bs=$bs, lr=$lr, n_layer=$n_layer, dropout=$dropout, weight_decay=$weight_decay, d_model=$d_model, batch_size=$batch_size"
-                python3 train.py \
-                    --block_size $bs \
-                    --lr $lr \
-                    --n_layer $n_layer \
-                    --dropout $dropout \
-                    --weight_decay $weight_decay \
-                    --d_model $d_model \
-                    --batch_size $batch_size
 
-            done
-          done
-        done
-      done
-    done
-  done
-done
+echo "Running with bs=$bs, lr=$lr, n_layer=$n_layer, dropout=$dropout, weight_decay=$weight_decay, d_model=$d_model, batch_size=$batch_size"
+python3 train.py \
+    --block_size 256 \
+    --lr 0.005 \
+    --n_layer 6 \
+    --dropout 0.15 \
+    --weight_decay 0.1 \
+    --d_model 256 \
+    --batch_size 32
+
+# for bs in 64 128 256; do
+#   for lr in 0.006 0.005 0.004 0.003; do
+#     for n_layer in 4 6 8; do
+#       for dropout in 0.1 0.2 0.3; do
+#         for weight_decay in 0.1 0.01 0.001; do
+#           for d_model in 128 256 512; do
+#             for batch_size in 16 32 64; do
+#               echo "Running with bs=$bs, lr=$lr, n_layer=$n_layer, dropout=$dropout, weight_decay=$weight_decay, d_model=$d_model, batch_size=$batch_size"
+#               python3 train.py \
+#                   --block_size $bs \
+#                   --lr $lr \
+#                   --n_layer $n_layer \
+#                   --dropout $dropout \
+#                   --weight_decay $weight_decay \
+#                   --d_model $d_model \
+#                   --batch_size $batch_size
+
+#             done
+#           done
+#         done
+#       done
+#     done
+#   done
+# done
