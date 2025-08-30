af84d27c202e4f2ed09bbb678eb34750c279e6ec
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-08-30 23:37:14 +1000

    345

---
+++ mainrun/model/attention/Causalattention.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from dataclasses import dataclass
+
+import math
+
+@dataclass
+class AttnConfig:
+    d_model: int
+    n_head: int
+    block_size: int
+    dropout: float
+    
+class CausalSelfAttention(nn.Module):
+    def __init__(self, cfg: AttnConfig):
+        super().__init__()
+        assert cfg.d_model % cfg.n_head == 0
+        self.head_dim = cfg.d_model // cfg.n_head
+        self.n_head   = cfg.n_head
+        self.qkv = nn.Linear(cfg.d_model, 3 * cfg.d_model)
+        self.proj = nn.Linear(cfg.d_model, cfg.d_model)
+        self.attn_drop = nn.Dropout(cfg.dropout)
+        self.resid_drop= nn.Dropout(cfg.dropout)
+        self.register_buffer("tril", torch.tril(torch.ones(cfg.block_size, cfg.block_size)))
+
+    def forward(self, x: torch.Tensor):
+        B, T, C = x.size()
+        qkv = self.qkv(x).view(B, T, 3, self.n_head, self.head_dim).transpose(1, 3)
+        q, k, v = qkv[..., 0, :, :], qkv[..., 1, :, :], qkv[..., 2, :, :]
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_drop(att)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        return self.resid_drop(self.proj(y))
--- mainrun/model/gpt.py
+++ mainrun/model/gpt.py
@@ -1,3 +1,4 @@
+from model.attention.Causalattention import CausalSelfAttention
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -5,7 +6,7 @@ from datasets import load_dataset
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders
 from dataclasses import dataclass
 from dataclasses import dataclass
-import math
+
 
 @dataclass
 class GPTConfig:
@@ -17,32 +18,6 @@ class GPTConfig:
     dropout: float
 
     
-
-    
-class CausalSelfAttention(nn.Module):
-    def __init__(self, cfg: GPTConfig):
-        super().__init__()
-        assert cfg.d_model % cfg.n_head == 0
-        self.head_dim = cfg.d_model // cfg.n_head
-        self.n_head   = cfg.n_head
-        self.qkv = nn.Linear(cfg.d_model, 3 * cfg.d_model)
-        self.proj = nn.Linear(cfg.d_model, cfg.d_model)
-        self.attn_drop = nn.Dropout(cfg.dropout)
-        self.resid_drop= nn.Dropout(cfg.dropout)
-        self.register_buffer("tril", torch.tril(torch.ones(cfg.block_size, cfg.block_size)))
-
-    def forward(self, x: torch.Tensor):
-        B, T, C = x.size()
-        qkv = self.qkv(x).view(B, T, 3, self.n_head, self.head_dim).transpose(1, 3)
-        q, k, v = qkv[..., 0, :, :], qkv[..., 1, :, :], qkv[..., 2, :, :]
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_drop(att)
-        y = att @ v
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
-        return self.resid_drop(self.proj(y))
-
 class MLP(nn.Module):
     def __init__(self, cfg: GPTConfig):
         super().__init__()
@@ -100,3 +75,4 @@ class GPT(nn.Module):
         else:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), reduction='mean')
         return logits, loss
+    
