8596b823269f6d2b4e9a4eab544df774648e5f9d
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-09-03 23:57:30 +1000

    commit train.sh with 1.324

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -111,4 +111,5 @@
 # echo "Running training script with specified hyperparameters..."
 # python3 train.py --batch_size=128 --dropout=0.1 --lr=0.012 --model_arhitecture=gpt --optimizer=sgd --weight_decay=0.01
 
-wandb agent arc_agi/gpt-from-scratch/${SWEEP_ID}
+# wandb agent arc_agi/gpt-from-scratch/${SWEEP_ID}
+python3 train.py --batch_size=128 --context_length=256 --d_model=128 --dropout=0.1 --lr=0.01 --n_layer=8 --optimizer=adamw --weight_decay=0.1
