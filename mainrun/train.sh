67805415d2e3176c9151c7270325ba502842f9e5
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-09-05 15:15:08 +1000

    updat train.sh path chnage

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 train.py --sweep --sweep_config "config?sweep_gpt_sparse.yaml" --orig_yaml "config/hparams_gpt_sparse.yaml"
+python3 train.py --sweep --sweep_config "./config/sweep_gpt_sparse.yaml" --orig_yaml "./config/hparams_gpt_sparse.yaml"
