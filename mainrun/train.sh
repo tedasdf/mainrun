3377a5090b3a7691a072a1163d8ca2f1301b64ef
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-09-05 19:39:24 +1000

    Update train.sh

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -2,4 +2,4 @@
 
 # python3 train.py --sweep --sweep_config "./config/sweep_gpt_sparse.yaml" --orig_yaml "./config/hparams_gpt_sparse.yaml"
 
-python3 train.py --test --orig_yaml config\hyperparams_unet.yaml
+python3 train.py --orig_yaml config\hyperparams_unet.yaml
