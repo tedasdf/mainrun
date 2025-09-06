2122437616521c090381caef52e97f54aab4b8fb
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-09-06 14:56:18 +1000

    update try

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -11,4 +11,4 @@
 
 
 # sweep with gpt and causal didnt work before too much memory ( hopefully its fixed )
-python train.py --sweep --sweep_config "./config/sweep_gpt.yaml" --orig_yaml "./config/hyperparams.yaml"
+python train.py --orig_yaml "./config/testhyperparams.yaml"
