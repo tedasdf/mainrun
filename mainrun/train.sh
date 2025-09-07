eee390f39dae4624753db9195f0a75b4d6ecd85e
Ted Lo <teedsingyau@gmail.com>
2025-09-07 01:03:13 +0000

    Mainrun auto checkpoint

--- mainrun/train.sh
+++ mainrun/train.sh
@@ -11,4 +11,8 @@
 
 
 # sweep with gpt and causal didnt work before too much memory ( hopefully its fixed )
-python train.py --sweep --sweep_config "./config/sweep_gpt.yaml" --orig_yaml "./config/hyperparams.yaml"
+# python train.py --sweep --sweep_config "./config/sweep_gpt.yaml" --orig_yaml "./config/hyperparams.yaml"
+
+
+
+python train.py --orig_yaml "./config/original_hyperparams.yaml"
